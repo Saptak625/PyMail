cc6735d88092caf216f632864ac6af2771a8ec0f
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-07-09 18:37:42 -0400

    Finish PyMail

--- main.py
+++ main.py
@@ -1,49 +1,111 @@
 # Importing libraries
 import imaplib
 import getpass
-from datetime import datetime
+from time import sleep
+from plyer import notification
+import pystray
+from PIL import Image
+ 
+image = Image.open("logo.png")
+
+tray_exit = False # The preferred way to exit the program is to click the tray icon for PyMail and select "Exit".
+
+def after_click(icon, query):
+    if str(query) == "Exit":
+        icon.stop()
+        global tray_exit
+        tray_exit = True
+ 
+ 
+icon = pystray.Icon("PyMail", image, "PyMail", menu=pystray.Menu(pystray.MenuItem("Exit", after_click)))
 
 # Credentials
+print('='*40+'Welcome to PyMail!'+'='*40)
+print('The preferred way to exit the program is to click the tray icon for PyMail and select "Exit".')
+print('-'*100)
+print('Enter your credentials:')
 user = input("Username: ")
 password = getpass.getpass("Password: ")
 imap_url = 'imap.gmail.com'
+
+delay = 10 # seconds
+notification_time = 5 # seconds
+
+# If either of these criteria are met, a push notification will be sent. If both are empty, all emails will be notified.
+# Priority 1: Emails from specific senders.
+priority_1 = []
+            
+# Priority 2: Emails with specific keywords.
+priority_2 = []
  
 # this is done to make SSL connection with GMAIL
-con = imaplib.IMAP4_SSL(imap_url)
- 
-# logging the user in
-con.login(user, password)
-print('Login Successful!')
+with imaplib.IMAP4_SSL(imap_url) as con:
+    # logging the user in
+    login_failed = False
+    try:
+        con.login(user, password)
+        print('Login Successful!\n')
+        icon.run_detached()
+    except imaplib.IMAP4.error:
+        print('Login Failed!\n')
+        login_failed = True
 
-# calling function to check for email under this label
-con.select('"[Gmail]/Sent Mail"')
+    if not login_failed:
+        # Get all unread emails
+        con.select('INBOX')
 
-# #Dates
-# date_format = "%d-%b-%Y"
-# startingDate = None
-# with open('last.txt', 'r') as f:
-#   startStr = f.read()
-#   startingDate = datetime.strptime(startStr, date_format) if startStr != 'None' else None
-# endingDate = datetime.strptime('8-May-2023', date_format)
+        email_ids = []
+        while not tray_exit:
+            # Check for new emails based on delay.
+            result, data = con.search(None, 'UNSEEN')
+            ids = data[0]
+            id_list = ids.split()
+            print('='*40+f'Found {len(id_list)} unread emails.'+'='*40)
 
-# # fetching emails from this user
-# hours = []
-# emailDates = {}
-# result, data = con.search(None, f'(SUBJECT "Time Card" TO "{os.environ["to_email"]}"'+(f' SENTSINCE {startingDate.strftime(date_format)}' if startingDate else '') + (f' BEFORE {endingDate.strftime(date_format)}' if endingDate else '') + ')')
-# for id in data[0].split():
-#   typ, subject = con.fetch(id, '(BODY.PEEK[HEADER])')
-#   subject=[i for i in str(subject[0]).split('\\r\\n') if 'Subject' in i][0].replace('Subject: ', '').strip('Re: ')
-#   resp, mailData = con.fetch(id, "(UID BODY[TEXT])")
-    
-#   #get the body of the email
-#   body = str(mailData[0][1]).split('--')[1].replace('Content-Type: text/plain;', '').split(':')
-#   hoursInWeek = []
-#   for t in body:
-#     if 'Saptak Das' in t: #Does not double count in replies.
-#       break
-#     if 'hours' in t:
-#       hoursInWeek.append(float(t.split('hours')[0].strip(' ')))
-#   if subject not in emailDates:
-#     emailDates[subject] = hoursInWeek
-#   else:
-#     emailDates[subject] += hoursInWeekpass
+            for i in id_list:
+                if i in email_ids:
+                    continue
+
+                # Peek at the email to get the sender and subject. Do not mark as read.
+                result, data = con.fetch(i, '(BODY.PEEK[HEADER])')
+                email_msg = data[0][1].decode('utf-8')
+                sender = email_msg[email_msg.find('From: ')+6:]
+                sender = sender[:sender.find('\n')]
+                subject = email_msg[email_msg.find('Subject: ')+9:]
+                subject = subject[:subject.find('\n')]
+                print('-'*100)
+                print(f'From: {sender}')
+                print(f'Subject: {subject}')
+
+                show_email = False
+                if not priority_1 and not priority_2:
+                    show_email = True # Show all emails if no criteria are specified.
+                else:
+                    # Check if email meets one of the criteria in priority 1 or 2.
+                    # Priority 1: Emails from specific senders.
+                    sender_email = sender[sender.find('<')+1:sender.find('>')]
+                    if priority_1 and sender_email in priority_1:
+                        show_email = True
+                    # Priority 2: Emails with specific keywords.
+                    elif priority_2 and any(keyword in subject for keyword in priority_2):
+                        show_email = True
+
+                # Show push notification if email meets criteria.
+                if show_email:
+                    print('Sending notification...')
+                    notification.notify(
+                        title=subject if len(subject) < 25 else subject[:25]+'...',
+                        message=f'From: {sender}',
+                        timeout=notification_time  # The notification will automatically close after 10 seconds
+                    )
+                else:
+                    print('Email does not meet criteria. Skipping...')
+
+                print('-'*100)
+
+                email_ids.append(i)
+
+            # Wait for delay before checking for new emails again.
+            sleep(delay)  
+        print('Exiting...')
+        icon.stop()
